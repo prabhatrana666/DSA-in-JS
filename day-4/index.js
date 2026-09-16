c116173563af80233b3038f2bdae2d3e069455b2
Prabhat Rana <153556000+prabhatrana666@users.noreply.github.com>
2026-09-16 17:08:46 +0530

    problem done

--- day-4/index.js
+++ day-4/index.js
@@ -1,4 +1,4 @@
-Problem statement
+Problem statement:
 You have been given an array ‘a’ of ‘n’ non-negative integers.You have to check whether the given array is sorted in the non-decreasing order or not.
 
 Your task is to return 1 if the given array is sorted. Else, return 0.
